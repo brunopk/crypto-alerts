48bf961d9dd45f2803a2b527d74ecb5531ed1709
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-06-22 01:40:33 -0300

    Update hello function to query Binance

---
+++ src/functions/main/handler.ts
@@ -0,0 +1,17 @@
+import 'source-map-support/register';
+import type { Handler, ScheduledEvent, Context, Callback } from "aws-lambda"
+import axios from "axios"
+
+
+export const main: Handler<ScheduledEvent> = async (event:ScheduledEvent, context:Context, callback:Callback) => {
+  const symbols = ['ADAUSD_PERP', 'ETHUSD_PERP']
+  const url = 'https://testnet.binancefuture.com/dapi/v1/premiumIndex'
+  const requests = symbols.map((value) => axios.get(`${url}?symbol=${value}`))
+  
+  try {
+    const result = await Promise.all(requests)
+    result.forEach((value) => console.log(value.data))
+  } catch(error) {
+    callback(error)
+  }
+}
