816cd83338f67519aa52743298438a80c2f012de
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-06-30 22:42:38 -0300

    Changes in main function and fixes for linting configuration problem with typescript imports aws-lambda

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -1,18 +1,31 @@
 import 'source-map-support/register';
-import type { Handler, ScheduledEvent, Context, Callback } from "aws-lambda"
-import axios from "axios"
+import type { Handler, ScheduledEvent, Context, Callback } from 'aws-lambda';
+import * as nodemailer from 'nodemailer';
+import axios from 'axios';
 
-
-export const main: Handler<ScheduledEvent> = async (_event:ScheduledEvent, _context:Context, callback:Callback) => {
-  
-  const symbols = ['ADAUSD_PERP', 'ETHUSD_PERP']
+// eslint-disable-next-line import/prefer-default-export
+export const main: Handler<ScheduledEvent> = async (
+  _event: ScheduledEvent,
+  _context: Context,
+  callback: Callback
+) => {
+  const symbols = ['ADAUSD_PERP', 'ETHUSD_PERP'];
   const url = 'https://testnet.binancefuture.com/dapi/v1/premiumIndex'
-  const requests = symbols.map((value) => axios.get(`${url}?symbol=${value}`))
-  
+  const requests = symbols.map((value) => axios.get(`${url}?symbol=${value}`));
   try {
-    const result = await Promise.all(requests)
-    result.forEach((value) => console.log(value.data))
-  } catch(error) {
-    callback(error)
+    const result = await Promise.all(requests);
+    result.forEach((value) => {
+      // https://edigleyssonsilva.medium.com/how-to-send-emails-securely-using-gmail-and-nodejs-eef757525324
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'cryptoalertjs@gmail.com',
+          pass: 'cryptoalert123',
+        },
+      });
+      transporter.verify().then(console.log).catch(console.error);
+    });
+  } catch (error) {
+    callback(error);
   }
-}
+};
